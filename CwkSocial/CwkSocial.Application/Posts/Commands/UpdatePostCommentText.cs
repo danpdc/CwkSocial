ad7b87f745a591a5a8f431669fb9cbdfa5357f2a
Vlad Mihet <vlad.mihet@amdaris.com>
2022-01-10 23:44:56 +0200

    feat: created comment aggregate, controller update request, handler & command

---
+++ CwkSocial/CwkSocial.Application/Posts/Commands/UpdatePostCommentText.cs
@@ -0,0 +1,14 @@
+﻿using Cwk.Domain.Aggregates.PostAggregate;
+using CwkSocial.Application.Models;
+using MediatR;
+
+namespace CwkSocial.Application.Posts.Commands;
+
+public class UpdatePostCommentText : IRequest<OperationResult<PostComment>>
+{
+    public string NewText { get; set; }
+
+    public Guid PostCommentId { get; set; }
+    public Guid PostId { get; set; }
+}
+
