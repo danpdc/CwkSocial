435490eeceb0d33db91a25cdd4c7ca9d1e830153
Luca Gabi <gabiluca@gmail.com>
2022-02-19 18:40:49 +0200

    fix: IJWTService interface

--- CwkSocial/CwkSocial.Api/Services/JWTService/IJWTService.cs
+++ CwkSocial/CwkSocial.Api/Services/JWTService/IJWTService.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
 
 namespace CwkSocial.Api.Services.JWTService
 {
     public interface IJWTService
     {
+        SecurityToken CreateSecurityToken(ClaimsIdentity identity);
         string GetJwtString(IdentityUser identityUser, UserProfile userProfile);
+        string WriteToken(SecurityToken token);
     }
 }
