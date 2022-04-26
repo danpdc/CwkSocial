99848af87dc03debeae1d21a636a46de58f0bfd0
Anton Kharchenko <gumb1t97@gmail.com>
2022-04-26 09:10:17 +0300

    :ambulance: fix(Api Layer): Improving Base Controller
    The common attributes and instances of IMapper and IMediator  were rendered to Base controller. The controllers which inherits Bace controller can get it.

--- CwkSocial/CwkSocial.Api/Controllers/V1/BaseController.cs
+++ CwkSocial/CwkSocial.Api/Controllers/V1/BaseController.cs
@@ -1,15 +1,23 @@
 ﻿namespace CwkSocial.Api.Controllers.V1;
 
+[ApiVersion("1.0")]
+[Route(ApiRoutes.BaseRoute)]
+[ApiController]
 public class BaseController : ControllerBase
 {
+    private IMediator _mediatorInstance;
+    private IMapper _mapperInstance;
+    protected IMediator _mediator => _mediatorInstance ??= HttpContext.RequestServices.GetService<IMediator>();
+    protected IMapper _mapper => _mapperInstance ??= HttpContext.RequestServices.GetService<IMapper>();
+
     protected IActionResult HandleErrorResponse(List<Error> errors)
     {
         var apiError = new ErrorResponse();
-        
+
         if (errors.Any(e => e.Code == ErrorCode.NotFound))
         {
             var error = errors.FirstOrDefault(e => e.Code == ErrorCode.NotFound);
-            
+
             apiError.StatusCode = 404;
             apiError.StatusPhrase = "Not Found";
             apiError.Timestamp = DateTime.Now;
@@ -17,8 +25,7 @@ public class BaseController : ControllerBase
 
             return NotFound(apiError);
         }
-        
-        
+
         apiError.StatusCode = 400;
         apiError.StatusPhrase = "Bad request";
         apiError.Timestamp = DateTime.Now;
--- CwkSocial/CwkSocial.Api/Controllers/V1/IdentityController.cs
+++ CwkSocial/CwkSocial.Api/Controllers/V1/IdentityController.cs
@@ -2,20 +2,8 @@
 
 namespace CwkSocial.Api.Controllers.V1;
 
-[ApiVersion("1.0")]
-[Route(ApiRoutes.BaseRoute)]
-[ApiController]
 public class IdentityController : BaseController
 {
-    private readonly IMediator _mediator;
-    private readonly IMapper _mapper;
-
-    public IdentityController(IMediator mediator, IMapper mapper)
-    {
-        _mediator = mediator;
-        _mapper = mapper;
-    }
-
     [HttpPost]
     [Route(ApiRoutes.Identity.Registration)]
     [ValidateModel]
